603fa109f3306004e1de6ec33b0dd3863196fd76
suhairshareef <suhairshareef1999@gmail.com>
2021-04-03 22:55:46 +0300

    Finalizing

--- DEFININGS.py
+++ DEFININGS.py
@@ -1,32 +1,32 @@
 # DEFININGs
 # The obtab for the SIC
 OPTAB = {
-	"ADD": "18",
-	"AND": "40",
-	"COMP": "28",
-	"DIV": "24",
-	"J": "3C",
-	"JEQ": "30",
-	"JGT": "34",
-	"JLT": "38",
-	"JSUB": "48",
-	"LDA": "00",
-	"LDCH": "50",
-	"LDL": "08",
-	"LDX": "04",
-	"MUL": "20",
-	"OR": "44",
-	"RD": "D8",
-	"RSUB": "4C",
-	"STA": "0C",
-	"STCH": "54",
-	"STL": "14",
-	"STSW": "E8",
-	"STX": "10",
-	"SUB": "1C",
-	"TD": "E0",
-	"TIX": "2C",
-	"WD": "DC"
+    "ADD": "18",
+    "AND": "40",
+    "COMP": "28",
+    "DIV": "24",
+    "J": "3C",
+    "JEQ": "30",
+    "JGT": "34",
+    "JLT": "38",
+    "JSUB": "48",
+    "LDA": "00",
+    "LDCH": "50",
+    "LDL": "08",
+    "LDX": "04",
+    "MUL": "20",
+    "OR": "44",
+    "RD": "D8",
+    "RSUB": "4C",
+    "STA": "0C",
+    "STCH": "54",
+    "STL": "14",
+    "STSW": "E8",
+    "STX": "10",
+    "SUB": "1C",
+    "TD": "E0",
+    "TIX": "2C",
+    "WD": "DC"
 }
 
 DIRECTIVES = [
@@ -40,9 +40,10 @@ DIRECTIVES = [
 ]
 
 ERRORLIST = [
-	"File is empty\n",
-	"Line not in fixed format\n",
-	"Duplicated label\n",
-	"Invalid operation code\n",
-	"Invalid data\n"
+    "File is empty\n",
+    "Line not in fixed format\n",
+    "Duplicated label\n",
+    "Invalid operation code\n",
+    "Invalid literal\n",
+    "Invalid data\n"
 ]
--- main.py
+++ main.py
@@ -3,13 +3,16 @@
 import DEFININGS
 
 # Read source file --> IT HAS TO BE .asm
-INPUT = open("source_file.asm", "r")
+INPUT = open("cource_file.asm", "r")
 
 # Output file --> intermediate file
 OUTPUT = open("intmdte_file.mdt", "w+")
 
 # Log file for errors --> errors file
 ERRORS = open("errors_file.txt", "w+")
+
+# Saving literal table
+LITTABLE = open("LITTABLE.txt", "w+")
 
 # Definings
 OPTAB = DEFININGS.OPTAB
@@ -22,7 +25,9 @@ PRGLTH = 0
 PRGNAME = ""
 ERRCTR = 0
 ADDSTA = 0
+LITPOOl = {}
 
+print("\n**************SIC ASSEMBLER*****************\n")
 # Reading from file
 # Reading first line
 line = INPUT.readline()
@@ -31,7 +36,7 @@ if line:
         PRGNAME = line[0:8].strip()  # get program name
         ADDSTA = int(line[16:35].strip(), 16)  # get the starting address and convert to int
         LOCCTR = ADDSTA
-        OUTPUT.write(hex(LOCCTR)[2:] + " " * (6 - len(str(LOCCTR))) + line)  # write first line in outputFile
+        OUTPUT.write(hex(LOCCTR)[2:] + " " * (10 - len(str(LOCCTR))) + line)  # write first line in outputFile
 
         # Continue reading
         while True:
@@ -39,7 +44,14 @@ if line:
             if not line:  # Check if there's no lines to read
                 break
             operation = line[9:15].strip()
+
             if operation != "END" and ("." not in line):  # if not endOfFile and the line is not a comment
+                if operation == "LTORG":
+                    OUTPUT.write(" " * 10 + line)
+
+                else:
+                    OUTPUT.write(hex(LOCCTR)[2:] + " " * (10 - len(str(LOCCTR))) + line)  # write line to outputFile
+
                 label = line[0:8].strip()  # get the label
 
                 if label != "":  # Check if there's a label]
@@ -50,14 +62,9 @@ if line:
 
                     else:  # if there's no errors add to the SYMTAB
                         SYMTAB[label] = hex(LOCCTR)[2:]
-                        OUTPUT.write(SYMTAB[label] + "          " + line[0:8].strip() + '\n')  # Write to the outputFile
 
-                operand = ""
-                if operation == "LTORG":
-                    OUTPUT.write(" " * 10 + line)
-
-                else:
-                    OUTPUT.write(hex(LOCCTR)[2:] + " " * (6 - len(str(LOCCTR))) + line)  # write line to outputFile
+                if operation not in OPTAB:
+                    operand = 0
 
                 # check if operation exist in OPTAB
                 if operation in OPTAB:
@@ -74,10 +81,10 @@ if line:
                     operand = line[16:35].strip()
 
                     if operand[0] == 'X':
-                       LOCCTR += int((len(operand) - 3)/2)
+                        LOCCTR += int((len(operand) - 3) / 2)
 
                     elif operand[0] == 'C':
-                        LOCCTR += (len(operand)-3)
+                        LOCCTR += (len(operand) - 3)
 
                 elif operation == "RESW":
                     operand = line[16:35].strip()
@@ -86,7 +93,7 @@ if line:
                 elif operation == "LTORG":
                     for i in LITTAB:
                         LITTAB[i][2] = hex(LOCCTR)[2:]
-                        OUTPUT.write(hex(LOCCTR)[2:]+" " * (6 - len(str(LOCCTR))) + "*" + " " * 7 + "=" + i + "\n")
+                        OUTPUT.write(hex(LOCCTR)[2:] + " " * (10 - len(str(LOCCTR))) + "*" + " " * 7 + "=" + i + "\n")
                         LOCCTR += int(LITTAB[i][1])
                     LITTAB = {}
 
@@ -99,26 +106,27 @@ if line:
                 if line[16:17] == '=':
                     exist = 1
                     literal = line[17:35].strip()
+
                     if literal[0] == 'X':
-                        hexco = literal[2:-1]
+                        hexcode = literal[2:-1]
+
                     elif literal[0] == 'C':
-                        hexco = literal[2:-1].encode("utf-8").hex()
+                        hexcode = literal[2:-1].encode("utf-8").hex()
 
                     else:
-                        print("NOT Valid Literal : " + " " + line[17:35].strip())
-                        ErrorFile.write("NOT Valid Literal : " + " " + line[17:35].strip())
-                        ErrorFile.write("\n")
+                        ERRORS.write(ERRORLIST[4])
+                        print(ERRORLIST[4])
+                        ERRCTR += 1
                         break
 
-                    if literal in litpool:
-                        exist = 0
+                    if literal not in LITPOOl:
+                        literals = [hexcode, len(hexcode) / 2, 0]
+                        LITTAB[literal] = literals
+                        LITPOOl[literal] = literals
+                        LITTABLE.write(str(LITPOOl[literal]) + "\n")
 
-                    else:
-                        literalList = [hexco, len(hexco) / 2, 0]
-                        littab[literal] = literalList
-                        litpool[literal] = literalList
-                        Littab.write(str(litpool[literal]))
-                        Littab.write("\n")
+            if operation == "END":
+                OUTPUT.write(" " * 10 + line)
 
     else:
         ERRORS.write(ERRORLIST[1])
@@ -128,3 +136,25 @@ else:
     ERRORS.write(ERRORLIST[0])
     print(ERRORLIST[0])
 
+if LITTAB:
+    for literal in LITTAB:
+        LITTAB[literal][2] = hex(LOCCTR)[2:]
+        OUTPUT.write(hex(LOCCTR)[2:] + " " * (10 - len(str(LOCCTR))) + "*" + " " * 7 + "=" + literal + "\n")
+        LOCCTR += int(LITTAB[literal][1])
+
+length = int(LOCCTR) - int(ADDSTA)
+PRGLTH = hex(int(length))[2:].format(int(length))
+loc = hex(int(LOCCTR))[2:].format(int(LOCCTR))
+INPUT.close()
+OUTPUT.close()
+LITTABLE.close()
+ERRORS.close()
+
+print("PROGRAM NAME: " + PRGNAME)
+print("PROGRAM LENGTH: " + str(PRGLTH).upper())
+print("LOCATION COUNTER: " + str(loc).upper())
+print("NUMBER OF ERRORS: " + str(ERRCTR) + "\n")
+
+print(" SYMBOL TABLE\n--------------")
+for label in SYMTAB:
+    print(label + " " * (10 - len(str(label))) + SYMTAB[label].upper())
